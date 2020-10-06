2ab7d000cda2cc83eded0abef9dde8d5fffee9d1
Nicolas <nicolas.7l@hotmail.com>
2020-10-06 15:48:37 -0300

    finalizando buscador de cep

--- App.js
+++ App.js
@@ -1,13 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, KeyboardEvent } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, SafeAreaView } from 'react-native';
+import api from './src/services/api'
 
 export default function App() {
+
+  const [cep, setCep] = useState('')
+
+  async function carregaCep(){
+    if(cep ==''){
+      return
+    }
+    await api.get(`${cep}/json/`)    
+   
+    .then(function(response){
+      setCep(response.data)
+
+    })
+    .catch(function(erro){
+      alert('Cep nao Localizado ! ')
+      console.log(erro)
+
+    })
+
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+
+      <Text style={styles.titulo}>Digite o CEP desejado: </Text>
+      <TextInput
+      style={styles.input}
+        placeholder='Ex: 09972010'
+        value={cep.cep}
+        onChangeText={(e) => setCep(e)}
+      />
+
+      <TouchableOpacity style={styles.btn} activeOpacity={0.5} onPress={()=>carregaCep()}>
+        <Text style={styles.btnTitulo}> Buscar CEP</Text>
+      </TouchableOpacity>
+
+      <Text>{cep.cep}</Text>
+      <Text>{cep.logradouro}</Text>
+      <Text>{cep.complemento}</Text>
+      <Text>{cep.bairro}</Text>
+      <Text>{cep.localidade}</Text>
+      <Text>{cep.uf}</Text>
+
+
+      <View></View>
+    </SafeAreaView>
   );
 }
 
@@ -18,4 +61,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  btn:{
+    margin:20,
+    width:250,
+    height:50,
+    backgroundColor:'#f345',
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:10
+  },
+  input:{
+    borderColor:'#000',
+    padding:8, 
+
+    margin:25    
+  },
+  titulo:{
+    fontSize:22,
+    fontWeight:'bold'
+  },
+  btnTitulo:{
+    fontWeight:'bold',
+    color:'#fff'
+  }
+  
 });
